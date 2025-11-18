e860e212acfdf6a95c0fc283ce1d3bdf2efd049a
Bart <bart.senn@gmail.com>
2025-11-18 21:04:42 +0100

    Fix foo test

--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -11,7 +11,7 @@ class GildedTrosTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedTros app = new GildedTros(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
 }
